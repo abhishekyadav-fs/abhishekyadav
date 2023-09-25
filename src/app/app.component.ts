a51815ba10d6c2dd7ff3b627b84d5c527f45a616
Abhishek Yadav <abhishekyadav@siemens.com>
2023-09-25 21:49:16 +0530

    First Version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'abhishekyadav';
+  title = 'Abhishek Yadav: fullstack developer';
+  
+  public open = () => {
+    window.open("../assets/abhishekyadav-resume.pdf","_blank");
+  }
 }
